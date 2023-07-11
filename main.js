ee72f156b0d7cd4c6d3fd22ba3ff5bef045f18e6
enverkaradede <enverkaradede@gmail.com>
2023-07-11 21:29:15 +0300

    initializing the project with React and Electron

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+
+const remoteMain = require("@electron/remote/main");
+remoteMain.initialize();
+
+const createWindow = () => {
+  // create the main app window from BrowserWindow
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+    },
+  });
+
+  // load React main page (index.html)
+  win.loadURL("http://localhost:3000");
+
+  // open chromeium dev tool in detached window mode
+  win.webContents.openDevTools({ mode: "detach" });
+
+  remoteMain.enable(win.webContents);
+};
+
+// open app whenever initializations done
+app.whenReady().then(() => {
+  createWindow();
+
+  // exception for Mac OS allows re-opening an application window
+  //   if the application would not closed by CMD + Q
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+// exception for Mac OS allows the application to run in background
+//   until user quits from the application with CMD + Q
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
